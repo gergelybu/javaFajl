bbe4d659ebdc8e69d5dbc27420f53a919e476654
gergelybu <118608660+gergelybu@users.noreply.github.com>
2023-05-18 18:28:16 +0200

    asd

---
+++ src/progtetelek/Fuvar.java
@@ -0,0 +1,77 @@
+
+package progtetelek;
+
+import java.text.NumberFormat;
+
+
+public class Fuvar {
+    private int taxi_id;
+    private String indulas;
+    private int idotartam;
+    private double tavolsag;
+    private double viteldij;
+    private double borravalo;
+    private String fizetes_modja;
+    
+    public Fuvar(String sor) {
+        //5240;2016-12-15 23:45:00;900;2,5;10,75;2,45;bankkártya
+        sor = sor.replace(',', '.'); //kicseréli a régi karaktert az újra
+        String[] s = sor.split(";");
+        this.taxi_id = Integer.parseInt(s[0]);
+        this.indulas = s[1];
+        this.idotartam = Integer.parseInt(s[2]);
+        //NumberFormat nf = NumberFormat.getInstance();
+        //Number num = nf.parse(s[3]);
+        //this.tavolsag = num.doubleValue();
+        this.tavolsag = Double.parseDouble(s[3]);    
+        this.viteldij = Double.parseDouble(s[4]);      
+        this.borravalo = Double.parseDouble(s[5]);
+        this.fizetes_modja = s[6];
+    }
+
+    public Fuvar(int taxi_id, String indulas, int idotartam, double tavolsag, double viteldij, double borravalo, String fizetes_modja) {
+        this.taxi_id = taxi_id;
+        this.indulas = indulas;
+        this.idotartam = idotartam;
+        this.tavolsag = tavolsag;
+        this.viteldij = viteldij;
+        this.borravalo = borravalo;
+        this.fizetes_modja = fizetes_modja;
+    }
+
+    public int getTaxi_id() {
+        return taxi_id;
+    }
+
+    public String getIndulas() {
+        return indulas;
+    }
+
+    public int getIdotartam() {
+        return idotartam;
+    }
+
+    public double getTavolsag() {
+        return tavolsag;
+    }
+
+    public double getViteldij() {
+        return viteldij;
+    }
+
+    public double getBorravalo() {
+        return borravalo;
+    }
+
+    public String getFizetes_modja() {
+        return fizetes_modja;
+    }
+
+    @Override
+    public String toString() {
+        return "Fuvar{" + "taxi_id=" + taxi_id + ", indulas=" + indulas + ", idotartam=" + idotartam + ", tavolsag=" + tavolsag + ", viteldij=" + viteldij + ", borravalo=" + borravalo + ", fizetes_modja=" + fizetes_modja + '}';
+    }
+    
+    
+    
+}
---
+++ src/progtetelek/Main.java
@@ -0,0 +1,101 @@
+package progtetelek;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+import java.util.Locale;
+
+public class Main {
+
+    private List<String> sorok;
+    private Fuvar[] fuvarok;
+
+    public static void main(String[] args) throws IOException {
+        new Main().feladatok();
+    }
+
+    public Main() throws IOException {
+        this.sorok = Files.readAllLines(Path.of("fuvar.csv"));
+        assert !sorok.isEmpty() : "üres a fájl";
+
+        fuvarok = new Fuvar[sorok.size() - 1];
+        for (int i = 0; i < sorok.size() - 1; i++) {
+            String sor = sorok.get(i + 1);
+            Fuvar fuvar = new Fuvar(sor);
+            fuvarok[i] = fuvar;
+            //fuvarok[i] = new Fuvar(sorok.get(i+1));
+        }
+        assert sorok.size() - 1 == fuvarok.length : "nincs meg minden fuvar!";
+        assert fuvarok[0] != null : "első fuvar hibás";
+        assert fuvarok[fuvarok.length - 1] != null : "utolsó fuvar hibás";
+        //project properties > run > VM: -enableassertions vagy -ea
+    }
+
+    private void feladatok() throws IOException {
+        feladat1();
+        feladat2();
+        feladat3();
+        feladat4();
+        feladat5();
+        feladat6();
+    }
+
+    private void feladat1() throws IOException {
+        //sorok = Files.readAllLines(Path.of("fuvar.csv"));
+        System.out.println("1. feladat: sorok száma");
+        System.out.printf("fájl, fejléccel együtt, %d sort tartalmaz\n", sorok.size()); //sorok száma
+    }
+
+    private void feladat2() throws IOException {
+        System.out.println("2. feladat: 1. fuvar adatai");
+        String sor = "5240;2016-12-15 23:45:00;900;2,5;10,75;2,45;bankkártya";
+        Fuvar fuvar = new Fuvar(sor);
+        System.out.println(fuvar);
+    }
+
+    private void feladat3() {
+        System.out.println("3. feladat: 1 véletlen fuvar adatai");
+        int sorszam = (int) ((Math.random() * (sorok.size() - 1)) + 1);
+        //sorszam = sorszam == 0 ? ++sorszam : sorszam;
+        String sor = sorok.get(sorszam);
+        Fuvar fuvar = new Fuvar(sor);
+        System.out.println(fuvar);
+    }
+
+    private void feladat4() {
+        System.out.println("4. feladat: milyen dátumú a legnagyobb távolságú fuvar:");
+        //maximum kiválasztás tétele
+        int max = 0;
+        for (int i = 1; i < fuvarok.length; i++) {
+            if (fuvarok[i].getTavolsag() > fuvarok[max].getTavolsag()) {
+                max = i;
+            }
+        }
+        System.out.println("távolság: " + fuvarok[max].getTavolsag());
+        System.out.println("dátum: " + fuvarok[max].getIndulas());
+    }
+
+    private void feladat5() {
+        System.out.println("5. feladat: borravalók átlaga: ");
+        //összegzés tétele
+        double osszeg = 0;
+        for (int i = 0; i < fuvarok.length; i++) {
+            osszeg += fuvarok[i].getBorravalo();
+        }
+        Locale loc = Locale.ENGLISH;
+        System.out.printf(loc, "%.2f \n", osszeg / fuvarok.length);
+    }
+
+    private void feladat6() {
+        System.out.println("6. feladat: minden fizetés bankkártyás-e: ");
+        //eldöntés tétele
+        int i = 0;
+        while (i < fuvarok.length && "bankkártya".equals(fuvarok[i].getFizetes_modja())) {
+            i++;
+        }
+        boolean mind = i >= fuvarok.length;
+        System.out.println(mind + " "+ i);
+    }
+
+}
